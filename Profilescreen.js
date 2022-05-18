a96ae4ed02d5c8e3bad6839078f2015528985004
thecodingking10 <67853976+thecodingking10@users.noreply.github.com>
2022-05-18 17:14:31 +0530

    done

---
+++ Profilescreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {TextInput, Text, TouchableOpacity, StyleSheet, View, ToastAndroid} from 'react-native'
+import db from '../config'
+import firebase from 'firebase'
+
+export default class ProfileScreen extends React.Component {
+    render() {
+        return(
+            <Text> Profile Screen </Text>
+        )
+    }
+}
---
+++ Sharingscreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {TextInput, Text, TouchableOpacity, StyleSheet, View, ToastAndroid} from 'react-native'
+import db from '../config'
+import firebase from 'firebase'
+
+export default class SharingScreen extends React.Component {
+    render() {
+        return(
+            <Text> Sharing Screen </Text>
+        )
+    }
+}
